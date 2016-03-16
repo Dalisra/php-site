69985bac41b134a0672a111cdad7d25242620c5e
Vytautas <vytautas@dalisra.com>
2016-03-16 21:51:29 +0100

    Do not need to include core resources. They are beeing autoloaded.

--- controllers/index.class.php
+++ controllers/index.class.php
@@ -4,8 +4,7 @@
  * Date: 18.02.14
  * Time: 21:55
  */
-require_once APP::$conf['path']['lib'] . 'app_controller.class.php';
-class Index_Controller  extends APP_Controller{
+class Index_Controller extends APP_Controller{
 
     //Overriding standard method with custom action.
     function process(){
